304cec066c785d85dd8d8fdc3ddf48e1ffeaaafb
schule <h.guzvic@gmail.com>
2022-11-28 21:07:28 +0100

    added my project

--- my_project/my_project.py
+++ my_project/my_project.py
@@ -1,6 +1,6 @@
 import customtkinter
 
-customtkinter.set_appearance_mode("dark")
+customtkinter.set_appearance_mode("light")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
